147e28b3b72e98b9e25a8786deb52ff113d56760
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-06-09 02:16:41 +0600

    complete bannaer section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
 const Home = () => {
     return (
-        <div>
-            <h1>this is home </h1>
+        <div className='overflow-hidden'>
+            <div class="hero w-screen h-screen bg-contain bg-no-repeat	" style={{ backgroundImage: 'url(https://i.ibb.co/ZJ20K9K/abdulahad.png)' }}>
+                <div class="hero-overlay bg-opacity-60"></div>
+                <div class="hero-content text-center text-neutral-content">
+                    <div class="max-w-md">
+                        <h1 class="mb-5 text-5xl font-bold">I'm Abdul Ahad</h1>
+                        <p class="mb-5">I Have 1-year of experiences Web Development sector. I learned many cores and modern things of teck from google and youtube, and also completed the course of programming hero.</p>
+                        <div className='flex justify-center gap-2'>
+                            <a href='https://drive.google.com/file/d/1Fm5Cl5D6R4HnhSaxsjgmk1MVPi3pxItG/view' class="btn btn-primary" target="_blank" rel='noreferrer'>See Resume</a>
+                            <button class="btn btn-primary">Hire Quickly</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
--- src/pages/shared/Navbar.js
+++ src/pages/shared/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = ({ children }) => {
     const menuItmes = <>
-        <li><a>Homea</a></li>
+        <li><a>Home</a></li>
         <li><a>Projects</a></li>
         <li><a>Contact Me</a></li>
 
@@ -13,7 +13,7 @@ const Navbar = ({ children }) => {
                 <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
                 <div class="drawer-content flex flex-col">
                     {/* <!-- Navbar --> */}
-                    <div class="w-full navbar bg-base-300">
+                    <div class="w-full navbar lg:px-20">
                         <div class="flex-none lg:hidden">
                             <label for="my-drawer-3" class="btn btn-square btn-ghost">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -35,7 +35,7 @@ const Navbar = ({ children }) => {
                 </div>
                 <div class="drawer-side">
                     <label for="my-drawer-3" class="drawer-overlay"></label>
-                    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
+                    <ul class="menu p-4 overflow-y-auto w-40 bg-base-100">
                         {
                             menuItmes
                         }
